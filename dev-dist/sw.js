56874cd4b7f655d0258cfff18c6f10f0afaa3ef3
Kaique Freitas <kaique.h.freitas@triforsys.com.br>
2024-07-01 08:53:19 -0300

    auto dev files

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fom4hrc9lag"
+    "revision": "0.oqldcqtu0ig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
