cba8423e52d81ea1b2e17a1b7749d4e3990c4bdf
Kaique Freitas <kaique.h.freitas@triforsys.com.br>
2024-07-30 14:09:19 -0300

    feat: change icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bm33qr9v4e8"
+    "revision": "0.9bdd6ih63p8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
