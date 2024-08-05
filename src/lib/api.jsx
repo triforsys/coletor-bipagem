b08d1f35beaa677168c5b79020ecf9641e566ae2
Kaique Freitas <kaique.h.freitas@triforsys.com.br>
2024-08-05 12:03:19 -0300

    feat: put prod url api

--- src/lib/api.jsx
+++ src/lib/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  // baseURL: 'http://localhost:3333/',
+  baseURL: 'https://api-bipagem.triforsys.com.br',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem('@Auth:token')}`,
