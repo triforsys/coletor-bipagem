81929af74bc00631474eddff256660e37a87e291
Kaique Freitas <kaique.h.freitas@triforsys.com.br>
2024-08-07 13:39:05 -0300

    feat: add navbar in auth routes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cf2o11i97u8"
+    "revision": "0.iotg56vap4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
