b10cd89a1a0e6f238d435e6f15a6ac07074ef67a
Renato Rebouças <renato190698@gmail.com>
2024-08-09 10:54:19 -0300

    wip

--- src/Pages/Region.jsx
+++ src/Pages/Region.jsx
@@ -1,3 +1,5 @@
+import { ButtonBack } from '@/components/layout/ButtonBack'
+import { Card } from '@/components/layout/Card'
 import Navbar from '@/components/layout/Navbar'
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -9,7 +11,17 @@ export default function Region() {
     <>
       <Navbar>
         <div className="flex justify-center gap-2 mt-4 font-poppins">
-          <div className="flex flex-wrap justify-center md:justify-normal p-4 gap-4 w-full xl:max-w-screen-xl"></div>
+          <div className="flex flex-wrap justify-center md:justify-normal p-4 gap-4 w-full xl:max-w-screen-xl">
+            <div className="w-full flex justify-center relative mb-6 items-center">
+              <ButtonBack />
+              <div className="flex justify-center">
+                <h1 className="text-2xl text-neutral-500">
+                  Coleta: {ordemColeta}
+                </h1>
+              </div>
+            </div>
+            <Card></Card>
+          </div>
         </div>
       </Navbar>
     </>
