4b7e547659def12a44873954022cdbd73bd0cdd2
Kaique Freitas <email@kaiquefreitas.com>
2024-12-05 13:49:37 -0300

    feat: summary screen

--- src/lib/api.jsx
+++ src/lib/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: 'http://localhost:3333/',
-  baseURL: 'https://api-bipagem.triforsys.com.br',
+  baseURL: 'http://localhost:3333',
+  // baseURL: 'https://api-bipagem.triforsys.com.br',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem('@Auth:token')}`,
